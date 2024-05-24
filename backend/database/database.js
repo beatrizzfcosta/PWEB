100dee841a91334463ed1a8bc45967d35e74a643
lukakrazy99 <luis_amandio99@hotmail.com>
2024-05-24 02:29:14 +0100

    database/server working fine update package

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,1 +1,16 @@
-//database stuff goes here like innit
+//database.js
+
+const mongoose = require("mongoose");
+const url = "mongodb+srv://droneProject:dronePassword@droneproject.twzxyqy.mongodb.net/";
+const dbName = "data";
+
+const connectDB = async () =>{
+    try{
+        await mongoose.connect(url, {dbName: dbName});
+
+    }catch (error){
+        console.error('Database error: ', error);
+    }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
 //server.js
 
+
 const app = require("./app");
+const connectDB = require('./database/database')
 const port = 15000;
 
-app.listen(port, () => {
-  console.log("+===========================================+");
-  console.log("|\x1b[32m   Server is up and running on port", port, "\x1b[0m |");
-  console.log("+===========================================+");
+const runServer = async () => {
+
+  await connectDB();
+
+  app.listen(port, () => {
+    console.log("+===========================================+");
+    console.log("|\x1b[32m   Server is up and running on port", port, "\x1b[0m |");
+    console.log("+===========================================+");
+  });
+
+};
+
+runServer().catch(error => {
+  console.error('Server error:', error);
 });
+
+
+
