074a07970052297ce7a62ff4c10c74ab76badf1e
lukakrazy99 <luis_amandio99@hotmail.com>
2024-05-28 23:01:38 +0100

    part get updated

--- backend/controllers/warehouse/parts/routes/get.js
+++ backend/controllers/warehouse/parts/routes/get.js
@@ -6,15 +6,33 @@ const Part = require("../../../../models/peca/peca");
 
 router.get("/", async (req, res) => {
   try {
-    const name = req.body.name;
 
-    const part = await Part.findOne({ name: name });
+    Part
+        .find()
+        .then((result)=>{
+          if(result.length === 0){
+            return res.json({
+              status: "fail",
+              message: "Part doesn't exists"
+            });
+          }
+          else{
+            return res.json({
+              status: "success",
+              message: "Part something something",
+              part: result,
+            });
 
-    return res.json({
-      status: "success",
-      message: "Part something something",
-      part: part,
-    });
+          }
+        })
+        .catch((error)=>{
+          console.error(error);
+          return res.status(400).json({
+            status: "fail",
+            message: "Error trying to find a part.",
+          });
+        });
+
   } catch (error) {
     console.log(error);
     return res.json({
