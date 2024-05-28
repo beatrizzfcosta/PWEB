4c37c1473e40ae11ebcbc223ece871aebc5292d9
lukakrazy99 <luis_amandio99@hotmail.com>
2024-05-28 21:56:30 +0100

    part post updated

--- backend/controllers/warehouse/parts/routes/post.js
+++ backend/controllers/warehouse/parts/routes/post.js
@@ -15,25 +15,55 @@ router.post("/", (req, res) => {
       });
     }
 
-    if (Part.findOne({name: name})) {
-      return res.status(400).json({
-        status: "fail",
-        message: "Part already exists",
-      });
-    }
-    
-    Part.create({
-      name,
-      type,
-      specifications,
-      quantity,
-    });
+      Part
+          .findOne({name: name})
+          .then((result)=>{
+              if(result!=null){
+                  return res.status(400).json({
+                      status: "fail",
+                      message: "Part already exists",
+                  });
+              }
+              else{
+                  Part
+                      .create({
+                      name,
+                      type,
+                      specifications,
+                      quantity,
+                  })
+                      .then(()=>{
+                          return res.json({
+                              status: "success",
+                              message: "Part created",
+                          });
+                      })
+                      .catch((error)=>{
+                          console.error(error);
+                          return res.status(400).json({
+                              status: "fail",
+                              message: "Error trying to create a new part.",
+                          });
+                      })
 
-    return res.json({
-      status: "success",
-      message: "Part created",
-    });
-  }catch{
+
+
+
+              }
+          })
+          .catch((error)=>{
+              console.error(error);
+              return res.status(400).json({
+                  status: "fail",
+                  message: "Error trying to find a part.",
+              });
+      });
+
+
+    
+
+  }catch(error){
+      console.error(error);
     return res.json({
       status: "fail",
       message: "Internal server error.",
