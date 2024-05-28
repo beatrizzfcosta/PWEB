97a24ada9cc4d7892e8f5ee1d5a6450e8e3ccf9c
lukakrazy99 <luis_amandio99@hotmail.com>
2024-05-28 23:10:19 +0100

    part patch updated

--- backend/controllers/warehouse/parts/routes/patch.js
+++ backend/controllers/warehouse/parts/routes/patch.js
@@ -4,15 +4,48 @@ const express = require("express");
 const router = express.Router();
 const Part = require("../../../../models/peca/peca");
 
-router.get("/", async (req, res) => {
+router.patch("/", async (req, res) => {
   try {
 
-    ////DB///////////////////////////
+    const {name, quantityUsed } = req.body;
+
+    if ( !name || !quantityUsed) {
+      return res.status(400).send({
+        status: 'fail',
+        message: "Can't find all necessary information"
+      });
+    }
+
+    const part = await Part.findOne({ name: name });
+
+    if (!part) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Part doesn't exist",
+      });
+    }
+
+    const updatedQuantity = part.quantity - quantityUsed;
+
+    if (updatedQuantity < 0) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Quantity cannot be negative',
+      });
+    }
+
+    await Part.updateOne(
+        { name: name },
+        { $set: { quantity: updatedQuantity } }
+    );
 
     return res.json({
       status: "success",
-      message: "Part patched",
+      message: "Part updated successfully",
+      updatedQuantity: updatedQuantity,
     });
+
+
   } catch (error) {
     return res.json({
       status: "fail",
