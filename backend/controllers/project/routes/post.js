8a176c8aae3abc414d1ef9de4200ede7dfbf87aa
rCodFish <elcfsantos@gmail.com>
2024-06-04 23:41:46 +0100

    project

--- backend/controllers/project/routes/post.js
+++ backend/controllers/project/routes/post.js
@@ -2,3 +2,41 @@
 
 const express = require("express");
 const router = express.Router();
+const Project = require("../../../models/project/project");
+
+router.post("/", async (req, res) => {
+  try {
+    const { name, drone, users, finished } = req.body;
+    const { user } = req; 
+
+    if (!name || !drone || !users || !finished) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Name, drone, users, and finished status are required fields.",
+      });
+    }
+
+    const newProject = await Project.create({
+      name: name,
+      drone: drone,
+      users: users,
+      finished: finished,
+    });
+
+    res.status(201).json({
+      status: "success",
+      message: "Project created successfully",
+      data: newProject,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      status: "fail",
+      message: "Internal server error.",
+    });
+  }
+});
+
+module.exports = router;
+
+
--- backend/controllers/warehouse/parts/routes/delete.js
+++ backend/controllers/warehouse/parts/routes/delete.js
@@ -9,25 +9,21 @@ router.delete("/", async (req, res) => {
     const name = req.body.name;
 
     await Part.deleteOne({ name: name })
-              .then((result)=>{
-                if(result.deletedCount === 0)
-                  res.status(400).json({
-                    status: "fail",
-                    message: "The specified part does not exist.",
-                  });
-                else
-                  res.json({
-                    status: "success",
-                    message: "Part deleted",
-                  });
-              })
-              .catch((error)=>{
-                console.error(error);
-              })
-
-
-
-
+      .then((result) => {
+        if (result.deletedCount === 0)
+          res.status(400).json({
+            status: "fail",
+            message: "The specified part does not exist.",
+          });
+        else
+          res.json({
+            status: "success",
+            message: "Part deleted",
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   } catch (error) {
     console.log(error);
     return res.json({
